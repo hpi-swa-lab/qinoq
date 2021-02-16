558191d7d146c5a6de0259097f2e0806e38899d9
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-02-16 10:26:10 +0100

    load sequences and add placeholder morphs in layers

--- editor.js
+++ editor.js
@@ -88,7 +88,8 @@ export class InteractivesEditor extends Morph {
   initializeSequenceTimeline (sequence) {
     const sequenceTimeline = new Timeline({ position: pt(0, CONSTANTS.SUBWINDOW_HEIGHT), extent: pt(CONSTANTS.EDITOR_WIDTH, CONSTANTS.EDITOR_HEIGHT - CONSTANTS.SUBWINDOW_HEIGHT), name: `${sequence.name} timeline` });
     this.addMorph(sequenceTimeline);
-    sequenceTimeline.loadSequence();
+    sequenceTimeline.initialize();
+    sequenceTimeline.loadSequence(sequence);
     return sequenceTimeline;
   }
 
