8177af63335fc4b045fa785ff06f3960832d5977
frcroth <frcroth@gmail.com>
2021-02-16 09:41:50 +0100

    Add semantic color names
    Co-authored-by: Paula-Kli <Paula-Kli@users.noreply.github.com>

--- colors.js
+++ colors.js
@@ -1,11 +1,10 @@
 import { Color } from 'lively.graphics';
 export const COLOR_SCHEME = {
-  BLUE: Color.rgb(0, 176, 255),
-  ORANGE: Color.rgb(240, 100, 0),
-  WHITE: Color.white,
-  GREY: Color.rgb(200, 200, 200),
-  BLACK: Color.black,
-  RED: Color.red,
-  TRANSPARENT: Color.transparent,
-  LIGHT_GREY: Color.rgb(240, 240, 240)
+  PRIMARY: Color.white,
+  SECONDARY: Color.black,
+  TERNARY: Color.rgb(220, 220, 220),
+  FOCUSSED: Color.rgb(0, 176, 255),
+  ACCENT: Color.rgb(240, 100, 0),
+  INVALID: Color.red,
+  TRANSPARENT: Color.transparent
 };
