7420ac6c4a6ee69da046fedcf1285908f15cffa5
Paula K <paula.klinke@student.hpi.de>
2021-04-20 12:11:24 +0200

    Make interactive freezable

--- interactive.js
+++ interactive.js
@@ -1,26 +1,27 @@
-import { Morph, ProportionalLayout, Image, Ellipse, Polygon } from 'lively.morphic';
-import { Color, pt } from 'lively.graphics';
+import { Morph, ProportionalLayout } from 'lively.morphic';
+import { pt } from 'lively.graphics';
 import { connect, disconnect, signal, disconnectAll } from 'lively.bindings';
 import { newUUID } from 'lively.lang/string.js';
 import { COLOR_SCHEME } from './colors.js';
-import { Keyframe, createAnimationForPropertyType, NumberAnimation, PointAnimation, ColorAnimation } from './index.js';
-import { LottieMorph } from './interactive-morphs/lottie-morph.js';
+
 import { arr } from 'lively.lang';
 
 export class Interactive extends Morph {
-  static example () {
+  static async example () {
     const interactive = new Interactive();
 
     const foregroundLayer = Layer.exampleForegroundLayer();
     const middleLayer = Layer.exampleMiddleLayer();
     const backgroundLayer = Layer.exampleBackgroundLayer();
-    const day = Sequence.backgroundDayExample();
+
+    const { backgroundDayExample, backgroundNightExample, treeExample, skyExample } = await System.import('qinoq/example.js');
+    const day = backgroundDayExample();
     day.layer = backgroundLayer;
-    const night = Sequence.backgroundNightExample();
+    const night = backgroundNightExample();
     night.layer = backgroundLayer;
-    const tree = Sequence.treeExample();
+    const tree = treeExample();
     tree.layer = middleLayer;
-    const sky = Sequence.skyExample();
+    const sky = skyExample();
     sky.layer = foregroundLayer;
     interactive.addLayer(backgroundLayer);
     interactive.addLayer(middleLayer);
@@ -565,89 +566,6 @@ export class Sequence extends Morph {
     return new Sequence({ name: 'first sequence', start: 0, duration: 250, ...props });
   }
 
-  static backgroundNightExample () {
-    const backgroundSequence = new Sequence({ name: 'night background', start: 0, duration: 250 });
-    const backgroundMorph = new Morph({ fill: Color.rgbHex('272a7c'), extent: pt(533, 300), name: 'night background' });
-    backgroundSequence.addMorph(backgroundMorph);
-    return backgroundSequence;
-  }
-
-  static backgroundDayExample () {
-    const backgroundSequence = new Sequence({ name: 'day background', start: 250, duration: 250 });
-    const backgroundMorph = new Morph({ fill: Color.rgbHex('60b2e5'), extent: pt(533, 300), name: 'day background' });
-    backgroundSequence.addMorph(backgroundMorph);
-
-    const sunrise = new Keyframe(0, Color.rgbHex('#ff4d00'), { name: 'sunrise' });
-    const daylight = new Keyframe(0.3, Color.rgbHex('60b2e5'), { name: 'daylight' });
-    const colorAnimation = new ColorAnimation(backgroundMorph, 'fill');
-    colorAnimation.addKeyframes([sunrise, daylight]);
-    backgroundSequence.addAnimation(colorAnimation);
-    return backgroundSequence;
-  }
-
-  static treeExample () {
-    const treeSequence = new Sequence({ name: 'tree sequence', start: 0, duration: 500 });
-    const stemMorph = new Morph({ fill: Color.rgbHex('734c30'), extent: pt(30, 60), name: 'stem' });
-    const vertices = [pt(60, 0), pt(90, 50), pt(70, 50), pt(100, 100), pt(70, 100), pt(110, 150), pt(10, 150), pt(50, 100), pt(20, 100), pt(50, 50), pt(30, 50)];
-    const crownMorph = new Polygon({ fill: Color.rgbHex('74a57f'), vertices: vertices, name: 'leafs' });
-    crownMorph.onMouseDown = () => {
-      crownMorph.fill = crownMorph.fill.darker();
-    };
-    crownMorph.onMouseUp = () => {
-      crownMorph.fill = crownMorph.fill.lighter();
-    };
-
-    treeSequence.addMorph(stemMorph);
-    treeSequence.addMorph(crownMorph);
-    stemMorph.position = pt(200, 220);
-    crownMorph.position = pt(165, 110);
-    return treeSequence;
-  }
-
-  static skyExample () {
-    const skySequence = new Sequence({ name: 'sky sequence', start: 0, duration: 500 });
-
-    const stars = new LottieMorph({ fill: Color.transparent, extent: pt(200, 200), position: pt(0, 0), name: 'lottie stars', animationDataUrl: 'https://assets4.lottiefiles.com/packages/lf20_Aerz0y.json' });
-    skySequence.addMorph(stars);
-
-    const starsOpacityAnimation = new NumberAnimation(stars, 'opacity');
-    starsOpacityAnimation.addKeyframes([new Keyframe(0, 1, { name: 'fully visible' }), new Keyframe(0.1, 0, { name: 'faded out' })]);
-    skySequence.addAnimation(starsOpacityAnimation);
-
-    const starProgressAnimation = new NumberAnimation(stars, 'progress');
-    starProgressAnimation.addKeyframes([new Keyframe(0, 0, { name: 'start of the animation' }), new Keyframe(0.1, 1, { name: 'animation done' })]);
-    skySequence.addAnimation(starProgressAnimation);
-
-    const sun = new Ellipse({ name: 'sun', extent: pt(70, 70), fill: Color.rgb(250, 250, 20), position: pt(0, 350) });
-    skySequence.addMorph(sun);
-
-    const sunPositionAnimation = new PointAnimation(sun, 'position', true);
-    sunPositionAnimation.addKeyframes([new Keyframe(0, pt(0, 1.2), { name: 'start' }), new Keyframe(0.5, pt(0.1, 0.27), { name: 'middle', easing: 'inQuad' }), new Keyframe(1, pt(0.45, 0.05), { name: 'end', easing: 'outCubic' })]);
-    skySequence.addAnimation(sunPositionAnimation);
-
-    const sunScaleAnimation = new NumberAnimation(sun, 'scale');
-    sunScaleAnimation.addKeyframes([new Keyframe(0, 0.6, { name: 'start' }), new Keyframe(0.6, 1, { name: 'end' })]);
-    skySequence.addAnimation(sunScaleAnimation);
-
-    const cloud = new Image({ name: 'cloud', extent: pt(100, 50), imageUrl: 'https://cdn.pixabay.com/photo/2017/06/20/04/42/cloud-2421760_960_720.png' });
-    skySequence.addMorph(cloud);
-    cloud.onHoverIn = () => {
-      cloud.blur = 3;
-    };
-    cloud.onHoverOut = () => {
-      cloud.blur = 0;
-    };
-    const cloudPositionAnimation = new PointAnimation(cloud, 'position', true);
-    cloudPositionAnimation.addKeyframes([new Keyframe(0, pt(0.25, 0.17), { name: 'start' }), new Keyframe(1, pt(0.5, 0.17), { name: 'end' })]);
-    skySequence.addAnimation(cloudPositionAnimation);
-
-    const cloudOpacityAnimation = new NumberAnimation(cloud, 'opacity');
-    cloudOpacityAnimation.addKeyframes([new Keyframe(0.1, 0, { name: 'start' }), new Keyframe(0.4, 1, { name: 'fully visible' })]);
-    skySequence.addAnimation(cloudOpacityAnimation);
-
-    return skySequence;
-  }
-
   get end () {
     return this.start + this.duration;
   }
@@ -706,13 +624,14 @@ export class Sequence extends Morph {
   }
 
   // Generic interface to add a keyframe to a sequence
-  addKeyframeForMorph (keyframe, morph, property, proptype = 'point') {
+  async addKeyframeForMorph (keyframe, morph, property, proptype = 'point') {
     const existingAnimation = this.getAnimationForMorphProperty(morph, property);
 
     if (existingAnimation) {
       existingAnimation.addKeyframe(keyframe);
       return existingAnimation;
     }
+    const { createAnimationForPropertyType } = await System.import('qinoq/animations.js');
     const newAnimation = createAnimationForPropertyType(proptype, morph, property);
     newAnimation.addKeyframe(keyframe);
     this.addAnimation(newAnimation);
