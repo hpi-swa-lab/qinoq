312779f125f1a40403b7b1654b02282c7584a136
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-04-21 15:33:27 +0200

    Add a test for hidden Sequences

--- interactive.js
+++ interactive.js
@@ -525,7 +525,7 @@ export class Sequence extends Morph {
         defaultValue: false,
         set (hidden) {
           this.setProperty('hidden', hidden);
-          this.interactive.redraw();
+          if (this.interactive) this.interactive.redraw();
         }
       }
     };
