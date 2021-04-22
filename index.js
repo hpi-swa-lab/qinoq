4ce70121b2ab481b89d7b020b5fc3376ac3dac43
Paula K <paula.klinke@student.hpi.de>
2021-04-22 13:14:24 +0200

    Add examples.js to index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 export { Interactive, Layer, Sequence } from './interactive.js';
 export { InteractivesEditor } from './editor.js';
 export { PointAnimation, NumberAnimation, createAnimationForPropertyType, ColorAnimation, Keyframe } from './animations.js';
+export { exampleInteractive } from './examples.js';
