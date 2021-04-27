8115ef88f871b3fe21543d649d3c552f7b8036cd
frcroth <frcroth@gmail.com>
2021-04-27 10:33:56 +0200

    Enable canvas serialization by introducing minimal width

--- timeline/index.js
+++ timeline/index.js
@@ -285,7 +285,7 @@ export class Timeline extends QinoqMorph {
 
   onActiveAreaWidthChange () {
     this.timelineLayers.forEach(timelineLayer => {
-      timelineLayer.activeArea.width = this._activeAreaWidth;
+      timelineLayer.activeArea.width = Math.max(this._activeAreaWidth, CONSTANTS.MINIMAL_ACTIVE_AREA_WIDTH);
     });
 
     const scrollbarWidth = this.ui.scrollBar.extent.x;
