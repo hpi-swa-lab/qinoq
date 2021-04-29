293dcaacdf296ce886aaac9a5a9d114d65adf50d
frcroth <frcroth@gmail.com>
2021-04-29 14:55:02 +0200

    Remove minimal active area width

--- timeline/index.js
+++ timeline/index.js
@@ -286,7 +286,7 @@ export class Timeline extends QinoqMorph {
 
   onActiveAreaWidthChange () {
     this.timelineLayers.forEach(timelineLayer => {
-      timelineLayer.activeArea.width = Math.max(this._activeAreaWidth, CONSTANTS.MINIMAL_ACTIVE_AREA_WIDTH);
+      timelineLayer.activeArea.width = this._activeAreaWidth;
     });
 
     const scrollbarWidth = this.ui.scrollBar.extent.x;
