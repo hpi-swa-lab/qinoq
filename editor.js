112f03d80f51c68331a3e9cb31fd930866297294
Paula K <paula.klinke@student.hpi.de>
2021-05-05 11:21:09 +0200

    Make lambdas command for icon buttons

--- editor.js
+++ editor.js
@@ -368,31 +368,6 @@ export class InteractivesEditor extends QinoqMorph {
     return this.getSequenceFor(this.ui.tabContainer.selectedTab);
   }
 
-  createNewSequence () {
-    if (!this.interactive) return;
-
-    // Assign a valid position to the new sequence
-    const lastSequenceInFirstLayer = this.interactive.getLastSequenceInLayer(this.interactive.layers[0]);
-    const startingPosition = lastSequenceInFirstLayer ? lastSequenceInFirstLayer.end : 0;
-
-    const newSequence = new Sequence({ name: 'unnamed sequence', start: startingPosition, duration: CONSTANTS.NEW_SEQUENCE_LENGTH });
-    newSequence.layer = this.interactive.layers[0];
-    this.interactive.addSequence(newSequence);
-
-    this.ui.globalTimeline.createTimelineSequenceInHand(newSequence);
-  }
-
-  createNewLayer () {
-    if (!this.interactive) return;
-
-    const newZIndex = this.interactive.highestZIndex + 10;
-    const newLayer = new Layer({ zIndex: newZIndex });
-
-    this.interactive.addLayer(newLayer);
-    this.ui.globalTimeline.createTimelineLayer(newLayer);
-    this.ui.globalTimeline.onActiveAreaWidthChange();
-  }
-
   addMorphToInteractive (morph) {
     this.currentSequence.addMorph(morph);
     this.onMorphAddition(morph); // Additional actions that are morph specific
@@ -550,6 +525,74 @@ export class InteractivesEditor extends QinoqMorph {
             const keyframe = allKeyframes[keyframeSearchStrings.indexOf(result.selected[0])];
             await this.goto(keyframe);
           }
+        }
+      },
+      {
+        name: 'scroll to start',
+        doc: 'set scrollposition to start of sequence of sequence view or start of interactive',
+        exec: () => {
+          this.interactiveScrollPosition = this.currentSequence ? this.currentSequence.start : 0;
+        }
+      },
+      {
+        name: 'scroll to previous sequence',
+        exec: () => {
+          const sequence = this.currentSequence;
+          const nextPosition = sequence ? sequence.getAbsolutePosition(sequence.getPrevKeyframePosition(sequence.progress)) : this.interactive.getPrevSequenceStart(this.interactiveScrollPosition);
+          if (nextPosition == undefined || isNaN(nextPosition)) return;
+          this.interactiveScrollPosition = nextPosition;
+        }
+      },
+      {
+        name: 'scroll to next sequence',
+        exec: () => {
+          const sequence = this.currentSequence;
+          const nextPosition = sequence ? sequence.getAbsolutePosition(sequence.getNextKeyframePosition(sequence.progress)) : this.interactive.getNextSequenceStart(this.interactiveScrollPosition);
+          if (nextPosition == undefined || isNaN(nextPosition)) return;
+          this.interactiveScrollPosition = nextPosition;
+        }
+      },
+      {
+        name: 'scroll to end',
+        doc: 'Scroll to the end of the interactive or the open sequence',
+        exec: () => {
+          this.interactiveScrollPosition = this.currentSequence ? this.currentSequence.end : this.interactive.length;
+        }
+      },
+      {
+        name: 'zoomToFitTimeline',
+        doc: 'Zoom so that the complete timeline can be seen',
+        exec: () => {
+          this.displayedTimeline.zoomToFit();
+        }
+      },
+      {
+        name: 'create new layer',
+        exec: () => {
+          if (!this.interactive) return;
+
+          const newZIndex = this.interactive.highestZIndex + 10;
+          const newLayer = new Layer({ zIndex: newZIndex });
+
+          this.interactive.addLayer(newLayer);
+          this.globalTimeline.createTimelineLayer(newLayer);
+          this.globalTimeline.onActiveAreaWidthChange();
+        }
+      },
+      {
+        name: 'create new sequence',
+        exec: () => {
+          if (!this.interactive) return;
+
+          // Assign a valid position to the new sequence
+          const lastSequenceInFirstLayer = this.interactive.getLastSequenceInLayer(this.interactive.layers[0]);
+          const startingPosition = lastSequenceInFirstLayer ? lastSequenceInFirstLayer.end : 0;
+
+          const newSequence = new Sequence({ name: 'unnamed sequence', start: startingPosition, duration: CONSTANTS.NEW_SEQUENCE_LENGTH });
+          newSequence.layer = this.interactive.layers[0];
+          this.interactive.addSequence(newSequence);
+
+          this.globalTimeline.createTimelineSequenceInHand(newSequence);
         }
       }];
   }
@@ -801,7 +844,7 @@ class MenuBar extends QinoqMorph {
     await this.buildIconButton({
       tooltip: 'Create a new sequence',
       target: this.editor,
-      action: 'createNewSequence',
+      command: 'create new sequence',
       icon: 'ticket-alt',
       label: 'New Sequence',
       name: 'addSequenceButton',
@@ -811,7 +854,7 @@ class MenuBar extends QinoqMorph {
     await this.buildIconButton({
       tooltip: 'Create a new layer',
       target: this.editor,
-      action: 'createNewLayer',
+      command: 'create new layer',
       icon: 'layer-group',
       label: 'New Layer',
       name: 'addLayerButton',
@@ -820,9 +863,8 @@ class MenuBar extends QinoqMorph {
 
     await this.buildIconButton({
       tooltip: 'Go to start',
-      action: () => {
-        this.editor.internalScrollChangeWithGUIUpdate(this.editor.currentSequence ? this.editor.currentSequence.start : 0);
-      },
+      target: this.editor,
+      command: 'scroll to start',
       icon: 'fast-backward',
       label: 'Go to start',
       collapsed: true,
@@ -832,12 +874,8 @@ class MenuBar extends QinoqMorph {
 
     await this.buildIconButton({
       tooltip: 'Go to previous sequence',
-      action: () => {
-        const sequence = this.editor.currentSequence;
-        const nextPosition = sequence ? sequence.getAbsolutePosition(sequence.getPrevKeyframePosition(sequence.progress)) : this.editor.interactive.getPrevSequenceStart();
-        if (nextPosition == undefined || isNaN(nextPosition)) return;
-        this.editor.internalScrollChangeWithGUIUpdate(nextPosition);
-      },
+      target: this.editor,
+      command: 'scroll to previous sequence',
       icon: 'step-backward',
       label: 'Go to previous sequence',
       collapsed: true,
@@ -849,12 +887,8 @@ class MenuBar extends QinoqMorph {
 
     await this.buildIconButton({
       tooltip: 'Go to next sequence',
-      action: () => {
-        const sequence = this.editor.currentSequence;
-        const nextPosition = sequence ? sequence.getAbsolutePosition(sequence.getNextKeyframePosition(sequence.progress)) : this.editor.interactive.getNextSequenceStart();
-        if (nextPosition == undefined || isNaN(nextPosition)) return;
-        this.editor.internalScrollChangeWithGUIUpdate(nextPosition);
-      },
+      target: this.editor,
+      command: 'scroll to next sequence',
       icon: 'step-forward',
       label: 'Go to next sequence',
       name: 'gotoNextButton',
@@ -864,9 +898,8 @@ class MenuBar extends QinoqMorph {
 
     await this.buildIconButton({
       tooltip: 'Go to end',
-      action: () => {
-        this.editor.internalScrollChangeWithGUIUpdate(this.editor.currentSequence ? this.editor.currentSequence.end : this.editor.interactive.length);
-      },
+      target: this.editor,
+      command: 'scroll to end',
       icon: 'fast-forward',
       label: 'Go to end',
       collapsed: true,
@@ -886,7 +919,8 @@ class MenuBar extends QinoqMorph {
 
     await this.buildIconButton({
       tooltip: 'Zoom to fit timeline',
-      action: () => this.editor.displayedTimeline.zoomToFit(),
+      target: this.editor,
+      action: 'zoomToFitTimeline',
       icon: 'expand-arrows-alt',
 	  label: 'Zoom to fit timeline',
       name: 'fitZoomButton',
@@ -895,7 +929,8 @@ class MenuBar extends QinoqMorph {
 
     await this.buildIconButton({
       tooltip: 'Find Keyframe',
-      action: () => this.editor.execCommand('find keyframe'),
+      target: this.editor,
+      command: 'find keyframe',
       icon: 'search-location',
       label: 'Find Keyframe',
       name: 'findKeyframeButton',
