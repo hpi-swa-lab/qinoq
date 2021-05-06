a80adb6b00ce5cff977951a09b56402c9d4a1a0e
frcroth <frcroth@gmail.com>
2021-05-06 11:28:31 +0200

    Move clipboard tests to seperate file

--- tests/editor.js
+++ tests/editor.js
@@ -370,35 +370,3 @@ describe('Editor and Interactive connections', () => {
     interactive.abandon();
   });
 });
-
-describe('Clipboard', () => {
-  let clipboard;
-  before(() => {
-    clipboard = new Clipboard();
-  });
-
-  it('holds a morph with animations for multiple accesses', () => {
-    const morph = 'Morph';
-    const animations = 'Animations';
-    clipboard.addMorph(morph, animations);
-    expect(clipboard.content).to.be.deep.equal({ morph, animations });
-    expect(clipboard.content).to.be.deep.equal({ morph, animations });
-  });
-
-  it('says it holds a morph', () => {
-    const morph = 'Morph';
-    const animations = 'Animations';
-    clipboard.addMorph(morph, animations);
-    expect(clipboard.containsMorph).to.be.ok;
-    clipboard.clear();
-    expect(clipboard.containsMorph).to.not.be.ok;
-  });
-
-  it('can be cleared', () => {
-    const morph = 'Morph';
-    const animations = 'Animations';
-    clipboard.addMorph(morph, animations);
-    clipboard.clear();
-    expect(clipboard.content).to.be.equal(null);
-  });
-});
