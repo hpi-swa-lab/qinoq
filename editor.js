48dff48e69a16bb635fb7aa1043d98e470f5334c
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-05-16 13:31:58 +0200

    Remove connection on `remove` when clearing the interactive

--- editor.js
+++ editor.js
@@ -271,7 +271,12 @@ export class InteractivesEditor extends QinoqMorph {
     this.interactive.clearMorphSequences();
     this.interactiveInEditMode = false;
 
-    this.interactive.withAllSubmorphsDo(submorph => { if (!submorph.isSequence && !submorph.isInteractive) disconnect(submorph, 'onAbandon', this, 'removeMorphFromInteractive'); });
+    this.interactive.withAllSubmorphsDo(submorph => {
+      if (!submorph.isSequence && !submorph.isInteractive) {
+        disconnect(submorph, 'onAbandon', this, 'removeMorphFromInteractive');
+        disconnect(submorph, 'remove', this, 'moveMorphOutOfInteractive');
+      }
+    });
 
     disconnect(this.interactive, 'onInternalScrollChange', this, 'onExternalScrollChange');
 
