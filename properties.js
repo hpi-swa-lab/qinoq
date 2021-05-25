a9d21dafb13a36f898412b2599aa4b99e9cab7fc
frcroth <frcroth@gmail.com>
2021-05-25 14:24:18 +0200

    Add default property colors

--- properties.js
+++ properties.js
@@ -1,51 +1,65 @@
 import { getColorForString, COLOR_SCHEME } from './colors.js';
+import { Color } from 'lively.graphics';
 
 export const animatedProperties = {
   extent: {
     type: 'point',
-    defaultRelative: true
+    defaultRelative: true,
+    color: Color.rgbHex('6447B3')
   },
   position: {
     type: 'point',
     flipCurve: true, // Flip curve in the animation curve in sequence layers
-    defaultRelative: true
+    defaultRelative: true,
+    color: Color.rgbHex('4792B3')
   },
   fill: {
     type: 'color',
     color: COLOR_SCHEME.BACKGROUND_VARIANT // light and neutral color so that the color curve is visible
   },
   blur: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('B34794')
   },
   flipped: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('B38247')
   },
   tilted: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('B35447')
   },
   grayscale: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('7647B3')
   },
   opacity: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('6FB347')
   },
   rotation: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('A6B347')
   },
   scale: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('B3476D')
   },
   textString: {
-    type: 'string'
+    type: 'string',
+    color: Color.rgbHex('D6C938')
   },
   fontSize: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('E9AFA3')
   },
   fontColor: {
-    type: 'color'
+    type: 'color',
+    color: COLOR_SCHEME.BACKGROUND_VARIANT
   },
   progress: {
-    type: 'number'
+    type: 'number',
+    color: Color.rgbHex('47B391')
   }
 
 };
