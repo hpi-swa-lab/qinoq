c3bb40ca4f22d7238258420da8b51233f8c4a537
frcroth <frcroth@gmail.com>
2021-05-25 18:10:54 +0200

    Collapse sequences in tree

--- tree.js
+++ tree.js
@@ -143,7 +143,7 @@ export class InteractiveGraph extends QinoqMorph {
     return {
       name: sequence.id,
       target: sequence,
-      isCollapsed: false,
+      isCollapsed: true,
       sequence: sequence,
       visible: true,
       container: this.buildContainerFor(sequence),
