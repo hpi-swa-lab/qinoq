e6b7dc13638a6d3db29dd7244293c80ba8feeb38
Silvan Verhoeven <silvan.verhoeven@student.hpi.de>
2021-05-28 23:04:34 +0200

    Adjust color palette

--- colors.js
+++ colors.js
@@ -1,22 +1,24 @@
 import { Color } from 'lively.graphics';
 import { string } from 'lively.lang';
 export const COLOR_SCHEME = {
-  PRIMARY: Color.rgb(0, 176, 255),
-  PRIMARY_VARIANT: Color.rgb(0, 72, 255),
-  PRIMARY_LIGHTER: Color.rgb(133, 193, 233),
-  SECONDARY: Color.rgb(240, 100, 0),
-  SECONDARY_VARIANT: Color.rgb(255, 160, 92),
+  PRIMARY: Color.rgb(0, 110, 230),
+  PRIMARY_VARIANT: Color.rgb(0, 93, 194),
+  SECONDARY: Color.rgb(255, 120, 0),
   ON_SECONDARY: Color.white,
   BACKGROUND: Color.white,
-  BACKGROUND_VARIANT: Color.rgb(200, 200, 200),
-  ON_BACKGROUND: Color.black,
-  ON_BACKGROUND_VARIANT: Color.rgb(220, 220, 220),
+  BACKGROUND_VARIANT: Color.rgb(230, 230, 230),
+  ON_BACKGROUND: Color.rgb(100, 100, 100),
+  ON_BACKGROUND_VARIANT: Color.rgb(200, 200, 200),
+  //
   PROMPT_BACKGROUND: Color.rgb(236, 240, 241),
   SURFACE: Color.white,
-  SURFACE_VARIANT: Color.rgb(220, 220, 220),
-  ON_SURFACE: Color.black,
-  ERROR: Color.rgb(210, 31, 44),
+  SURFACE_VARIANT: Color.rgb(245, 245, 245),
+  SURFACE_DARKER_VARIANT: Color.rgb(230, 230, 230),
+  ON_SURFACE: Color.white,
+  ON_SURFACE_VARIANT: Color.rgb(100, 100, 100),
+  ERROR: Color.rgb(255, 0, 14),
   TRANSPARENT: Color.transparent,
+  //
   KEYFRAME_FILL: Color.rgb(134, 134, 134),
   KEYFRAME_BORDER: Color.rgb(69, 69, 69),
   BUTTON_BLUE: Color.rgb(0, 110, 230)
