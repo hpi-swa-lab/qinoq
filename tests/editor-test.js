bf5f898002abc4d0f77489d4a572dcdd1af9f599
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-06-22 11:34:31 +0200

    Use delete key to remove sequences and keyframes instead of backspace

--- tests/editor-test.js
+++ tests/editor-test.js
@@ -268,11 +268,11 @@ describe('Editor', () => {
       expect(editor.interactive.scrollPosition).to.be.equal(51);
     });
 
-    it('can delete a sequence via Backspace key', () => {
+    it('can delete a sequence via Delete key', () => {
       const nightBackgroundSequence = interactive.sequences.find(sequence => sequence.name == 'night background');
       const nightBackgroundTimelineSequence = timelineSequences().find(timelineSequence => timelineSequence.sequence == nightBackgroundSequence);
       nightBackgroundTimelineSequence.isSelected = true;
-      editor.simulateKeys('Backspace');
+      editor.simulateKeys('Delete');
       expect(nightBackgroundTimelineSequence.world()).to.not.be.ok;
       expect(interactive.sequences).to.not.include(nightBackgroundSequence);
     });
