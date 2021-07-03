77626d404e146f623be47704746fdeac4985037f
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-07-03 17:17:35 +0200

    Support position and fill Animations

--- web-animations.js
+++ web-animations.js
@@ -1,20 +1,42 @@
-export class WebAnimationPointAnimation {
-  constructor (targetMorph) {
+export class WebAnimation {
+  constructor (targetMorph, property) {
     this.target = targetMorph;
+    this.property = property;
     this.keyframes = [];
     this.webAnimation = null;
   }
 
-  // currently accepts EXACTLY two keyframes, one for the beginning and one for the end of the animation
-  // provide keyframes in the correct order
+  // Accepts EXACTLY two keyframes, one for the beginning and one for the end of the animation.
+  // Provide Keyframes in the correct order.
   addKeyframes (keyframes) {
-    this.keyframes = keyframes; // who knows what they might be good for
-    const xOffset = this.keyframes[1].value.x - this.keyframes[0].value.x;
-    const yOffset = this.keyframes[1].value.y - this.keyframes[0].value.y;
-    this._keyframes = [
-      { transform: 'translate(0px,0px)' }, // beginning
-      { transform: `translate(${xOffset}px,${yOffset}px)` } // end
-    ];
+    this.keyframes = keyframes;
+    this._keyframes = this.generateCSSKeyframes();
+  }
+
+  generateCSSKeyframes () {
+    debugger;
+    switch (this.property) {
+      case 'position':
+        const xOffset = this.keyframes[1].value.x - this.keyframes[0].value.x;
+        const yOffset = this.keyframes[1].value.y - this.keyframes[0].value.y;
+        return [
+          { transform: 'translate(0px,0px)' }, // beginning
+          { transform: `translate(${xOffset}px,${yOffset}px)` } // end
+        ];
+        this.translate = 'add';
+        break;
+      case 'fill':
+        const c1 = this.keyframes[0].value;
+        const c2 = this.keyframes[1].value;
+        return [
+          { backgroundColor: `${this.keyframes[0].value}` }, // beginning
+          { backgroundColor: `${this.keyframes[1].value}` } // end
+        ];
+        this.translate = 'add';
+        break;
+      default:
+        throw 'Provide Valid Property to Animate';
+    }
   }
 
   set progress (progress) {
