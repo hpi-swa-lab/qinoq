4e81efc5153b9ef8d8222d6a642c10a490a87794
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-07-26 12:17:46 +0200

    Add Scrollytelling class

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'format esm';
 export { LottieMorph } from './interactive-morphs/lottie-morph.js';
-export { Interactive, Layer, Sequence } from './interactive.js';
+export { Interactive, Layer, Sequence, Scrollytelling } from './interactive.js';
 export { InteractivesEditor } from './editor.js';
 export { PointAnimation, NumberAnimation, createAnimationForPropertyType, ColorAnimation, Keyframe } from './animations.js';
 export { exampleInteractive } from './examples.js';
